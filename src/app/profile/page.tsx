569e5dd7bc774b980b517963c7ee939d313da1c4
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-01-21 13:40:26 +0530

    added middleWare and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,42 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function ProfilePage() {
+    const router = useRouter();
+    const [data, setData] = useState("nothing")
+    const logout = async () => {
+        try{
+            await axios.get('/api/users/logout');
+            toast.success('Logout Successful');
+            router.push('/login');
+        }catch(error: any){
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }
+
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
+
+
+
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2>{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+            <hr />
+            <button className="flex-none rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500" onClick={logout}>Logout</button>
+            <button className="flex-none rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500 mt-4" onClick={getUserDetails}>Get User details</button>
         </div>
     )
 }
